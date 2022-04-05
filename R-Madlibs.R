b51fe042cda6a56b7220dfa07f5fde7098360f7a
melaniewalsh <melanie.walsh@cornell.edu>
2022-04-05 00:19:19 -0700

    update assignment

--- R-Madlibs.R
+++ R-Madlibs.R
@@ -2,13 +2,13 @@
 
 # Talk to your partners and ask them about their hometown, favorite food, age, and whether they like rain
 
-# Assign the appropriate values to each of the following variable names. Make sure to run each line to save the variable in your Global Environment
-hometown
-food
-age
-likes_rain
+# Assign the appropriate values to each of the following variable names, and use the data type listed in the comment. Make sure to run each line to save the variable in your Global Environment
+hometown                #character
+food                   #character
+age                     #numeric
+likes_rain             #logical
 # Calculate someone's likely birth year only by using the age variable and a basic math operation
-birth_year
+birth_year              #numeric
 
 
 # Use a built-in R function to mash together each character string and variable, and print the message to the console
